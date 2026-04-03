164bd42a76b3b9c8923ca3a5ce273016d274a00b
Edison-Tong <edisonjtong@gmail.com>
2026-04-03 11:20:19 -0600

    added backend url config

---
+++ src/config/api.js
@@ -0,0 +1,4 @@
+export const API_BASE_URL = "https://bahoth-server.onrender.com";
+
+// Useful once WebSocket multiplayer wiring is added.
+export const WS_BASE_URL = API_BASE_URL.replace(/^http/i, "ws");
